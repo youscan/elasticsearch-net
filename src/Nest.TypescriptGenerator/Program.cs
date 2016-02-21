4d4551ee58d1b469c227440a8ed7b41b9b7d1b30
Russ Cam <russ.cam@elastic.co>
2016-02-21 13:57:09 -0800

    generate typescript interfaces for all requests and responses

---
+++ src/Nest.TypescriptGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TypeLite;
+using TypeLite.Net4;
+
+namespace Nest.TypescriptGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+	        var nestInterfaces = typeof (IRequest).Assembly
+		        .GetTypes()
+		        .Where(t => typeof(IRequest).IsAssignableFrom(t) || typeof(IResponse).IsAssignableFrom(t))
+				.Where(t => t.IsClass)
+		        .ToArray();
+
+	        var definitions = nestInterfaces.Aggregate(TypeScript.Definitions(), (def, t) => def.For(t));
+
+	        if (!Directory.Exists(@"c:\temp"))
+	        {
+		        Directory.CreateDirectory(@"c:\temp");
+	        }
+
+			File.WriteAllText(@"c:\temp\interfaces.ts", definitions.Generate());
+        }
+    }
+}
