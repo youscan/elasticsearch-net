8f0823eb00273f4ac10d6a5d7b7c1040fa66e6ba
Mpdreamz <mpdreamz@gmail.com>
2016-02-21 15:34:25 -0800

    formatting

--- src/Nest.TypescriptGenerator/Program.cs
+++ src/Nest.TypescriptGenerator/Program.cs
@@ -10,43 +10,45 @@ using TypeLite.Net4;
 
 namespace Nest.TypescriptGenerator
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-	        var nestInterfaces = typeof (IRequest).Assembly
-		        .GetTypes()
-		        .Where(t => typeof(IRequest).IsAssignableFrom(t) || typeof(IResponse).IsAssignableFrom(t))
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var nestInterfaces = typeof(IRequest).Assembly
+				.GetTypes()
+				.Where(t => typeof(IRequest).IsAssignableFrom(t) || typeof(IResponse).IsAssignableFrom(t))
 				.Where(t => t.IsClass && !t.Name.EndsWith("Descriptor"))
-		        .ToArray();
+				.ToArray();
 
-			var definitions = nestInterfaces.Aggregate(TypeScript.Definitions(),
-				(def, t) => def.For(t)
-							   .WithMemberFormatter(i =>
-							   {
-								   var declaringType = i.MemberInfo.DeclaringType;
-								   var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
-								   if (iface == null)
-									   return i.MemberInfo.Name;
-								   var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
-								   if (ifaceProperty == null)
-									   return i.MemberInfo.Name;
-								   var jsonPropertyAttribute = ifaceProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
-								   if (jsonPropertyAttribute != null)
-									   return jsonPropertyAttribute.PropertyName;
-								   jsonPropertyAttribute = i.MemberInfo.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
-								   if (jsonPropertyAttribute != null)
-									   return jsonPropertyAttribute.PropertyName;
-								   return i.MemberInfo.Name;
-							   })
+			var definitions = nestInterfaces.Aggregate(
+				TypeScript.Definitions()
+					.WithMemberFormatter(i =>
+					{
+						var declaringType = i.MemberInfo.DeclaringType;
+
+						var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
+						if (iface == null)
+						return i.MemberInfo.Name;
+						var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
+						if (ifaceProperty == null)
+						return i.MemberInfo.Name;
+						var jsonPropertyAttribute = ifaceProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
+						if (jsonPropertyAttribute != null)
+						return jsonPropertyAttribute.PropertyName;
+						jsonPropertyAttribute = i.MemberInfo.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
+						if (jsonPropertyAttribute != null)
+						return jsonPropertyAttribute.PropertyName;
+						return i.MemberInfo.Name;
+					})
+				, (def, t) => def.For(t)
 			);
 
-	        if (!Directory.Exists(@"c:\temp"))
-	        {
-		        Directory.CreateDirectory(@"c:\temp");
-	        }
+			if (!Directory.Exists(@"c:\temp"))
+			{
+				Directory.CreateDirectory(@"c:\temp");
+			}
 
 			File.WriteAllText(@"c:\temp\interfaces.ts", definitions.Generate());
-        }
-    }
+		}
+	}
 }
