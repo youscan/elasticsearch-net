a151b8c0b7e38d0224e622b7397ab1f443110630
Russ Cam <russ.cam@elastic.co>
2016-02-21 15:57:43 -0800

    Modify TypeLite to include TinyTypes for all numeric types

--- src/Nest.TypescriptGenerator/Program.cs
+++ src/Nest.TypescriptGenerator/Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TypeLite;
-using TypeLite.Net4;
+using TypeLite.TsModels;
 
 namespace Nest.TypescriptGenerator
 {
@@ -20,26 +20,12 @@ namespace Nest.TypescriptGenerator
 				.Where(t => t.IsClass && !t.Name.EndsWith("Descriptor"))
 		        .ToArray();
 
-			var definitions = nestInterfaces.Aggregate(TypeScript.Definitions(),
-				(def, t) => def.For(t)
-							   .WithMemberFormatter(i =>
-							   {
-								   var declaringType = i.MemberInfo.DeclaringType;
-								   var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
-								   if (iface == null)
-									   return i.MemberInfo.Name;
-								   var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
-								   if (ifaceProperty == null)
-									   return i.MemberInfo.Name;
-								   var jsonPropertyAttribute = ifaceProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
-								   if (jsonPropertyAttribute != null)
-									   return jsonPropertyAttribute.PropertyName;
-								   jsonPropertyAttribute = i.MemberInfo.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
-								   if (jsonPropertyAttribute != null)
-									   return jsonPropertyAttribute.PropertyName;
-								   return i.MemberInfo.Name;
-							   })
-			);
+	        var typeScriptFluent = TypeScript.Definitions()
+		        .WithMemberFormatter(FormatMember)
+		        .WithVisibility((@class, name) => false)
+				.WithModuleNameFormatter(module => "Elasticsearch");
+
+			var definitions = nestInterfaces.Aggregate(typeScriptFluent, (def, t) => def.For(t));
 
 	        if (!Directory.Exists(@"c:\temp"))
 	        {
@@ -48,5 +34,27 @@ namespace Nest.TypescriptGenerator
 
 			File.WriteAllText(@"c:\temp\interfaces.ts", definitions.Generate());
         }
+
+	    private static string FormatMember(TsProperty property)
+	    {
+			    var declaringType = property.MemberInfo.DeclaringType;
+			    var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
+			    if (iface == null)
+				    return property.MemberInfo.Name;
+			    var ifaceProperty = iface.GetProperty(property.MemberInfo.Name);
+			    if (ifaceProperty == null)
+				    return property.MemberInfo.Name;
+			    var jsonPropertyAttribute =
+				    ifaceProperty.GetCustomAttributes(typeof (JsonPropertyAttribute), true).FirstOrDefault() as
+					    JsonPropertyAttribute;
+			    if (jsonPropertyAttribute != null)
+				    return jsonPropertyAttribute.PropertyName;
+			    jsonPropertyAttribute =
+				    property.MemberInfo.GetCustomAttributes(typeof (JsonPropertyAttribute), true).FirstOrDefault() as
+					    JsonPropertyAttribute;
+			    if (jsonPropertyAttribute != null)
+				    return jsonPropertyAttribute.PropertyName;
+			    return property.MemberInfo.Name;
+	    }
     }
 }
