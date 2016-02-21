0ddf0d0cc9b7572082b2a53bec150faf00d6315a
Greg Marzouka <greg.marzouka@gmail.com>
2016-02-21 14:14:11 -0800

    Use the JsonProperty.PropertyName as the typescript properties

--- src/Nest.TypescriptGenerator/Program.cs
+++ src/Nest.TypescriptGenerator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,10 +17,29 @@ namespace Nest.TypescriptGenerator
 	        var nestInterfaces = typeof (IRequest).Assembly
 		        .GetTypes()
 		        .Where(t => typeof(IRequest).IsAssignableFrom(t) || typeof(IResponse).IsAssignableFrom(t))
-				.Where(t => t.IsClass)
+				.Where(t => t.IsClass && !t.Name.EndsWith("Descriptor"))
 		        .ToArray();
 
-	        var definitions = nestInterfaces.Aggregate(TypeScript.Definitions(), (def, t) => def.For(t));
+			var definitions = nestInterfaces.Aggregate(TypeScript.Definitions(),
+				(def, t) => def.For(t)
+							   .WithMemberFormatter(i =>
+							   {
+								   var declaringType = i.MemberInfo.DeclaringType;
+								   var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
+								   if (iface == null)
+									   return i.MemberInfo.Name;
+								   var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
+								   if (ifaceProperty == null)
+									   return i.MemberInfo.Name;
+								   var jsonPropertyAttribute = ifaceProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
+								   if (jsonPropertyAttribute != null)
+									   return jsonPropertyAttribute.PropertyName;
+								   jsonPropertyAttribute = i.MemberInfo.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
+								   if (jsonPropertyAttribute != null)
+									   return jsonPropertyAttribute.PropertyName;
+								   return i.MemberInfo.Name;
+							   })
+			);
 
 	        if (!Directory.Exists(@"c:\temp"))
 	        {
