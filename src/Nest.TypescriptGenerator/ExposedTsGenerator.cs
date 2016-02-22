dee06d848b790c5de592a085d9656b8d2314caa1
Russ Cam <russ.cam@elastic.co>
2016-02-22 11:05:08 -0800

    output EnumMemberAttribute values for enum values

--- src/Nest.TypescriptGenerator/ExposedTsGenerator.cs
+++ src/Nest.TypescriptGenerator/ExposedTsGenerator.cs
@@ -1,9 +1,41 @@
+using System.Reflection;
+using System.Runtime.Serialization;
 using TypeLite;
+using TypeLite.TsModels;
 
 namespace Nest.TypescriptGenerator
 {
 	public class ExposedTsGenerator : TsGenerator
 	{
 		public TypeConvertorCollection Converters => base._typeConvertors;
+
+		protected override void AppendEnumDefinition(TsEnum enumModel, ScriptBuilder sb, TsGeneratorOutput output)
+		{
+			string typeName = this.GetTypeName(enumModel);
+			string visibility = string.Empty; //(output & TsGeneratorOutput.Enums) == TsGeneratorOutput.Enums || (output & TsGeneratorOutput.Constants) == TsGeneratorOutput.Constants ? "export " : "";
+
+			_docAppender.AppendEnumDoc(sb, enumModel, typeName);
+
+			string constSpecifier = this.GenerateConstEnums ? "const " : string.Empty;
+			sb.AppendLineIndented(string.Format("{0}{2}enum {1} {{", visibility, typeName, constSpecifier));
+
+			using (sb.IncreaseIndentation())
+			{
+				int i = 1;
+				foreach (var v in enumModel.Values)
+				{
+					_docAppender.AppendEnumValueDoc(sb, v);
+					var enumMemberAttribute = v.Field.GetCustomAttribute<EnumMemberAttribute>();
+					var name = !string.IsNullOrEmpty(enumMemberAttribute?.Value) ? enumMemberAttribute.Value : v.Name;
+
+					sb.AppendLineIndented(string.Format(i < enumModel.Values.Count ? "{0} = {1}," : "{0} = {1}", name, v.Value));
+					i++;
+				}
+			}
+
+			sb.AppendLineIndented("}");
+
+			_generatedEnums.Add(enumModel);
+		}
 	}
 }
