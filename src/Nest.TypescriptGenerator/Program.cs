fba2b9485a96dd6f5abf215c7bd498433f3cd598
Mpdreamz <mpdreamz@gmail.com>
2016-02-21 16:21:54 -0800

    added requestparameters lookup

--- src/Nest.TypescriptGenerator/Program.cs
+++ src/Nest.TypescriptGenerator/Program.cs
@@ -4,9 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TypeLite;
 using TypeLite.Net4;
+using Elasticsearch.Net;
 
 namespace Nest.TypescriptGenerator
 {
@@ -14,30 +16,49 @@ namespace Nest.TypescriptGenerator
 	{
 		public static void Main(string[] args)
 		{
-			var nestInterfaces = typeof(IRequest).Assembly
+			var isDescriptorRe = new Regex(@"Descriptor(?:\<.+$|$)");
+
+			var nestAssembly = typeof(IRequest<>).Assembly;
+			var lowLevelAssembly = typeof(IElasticLowLevelClient).Assembly;
+			var nestInterfaces = nestAssembly
 				.GetTypes()
 				.Where(t => typeof(IRequest).IsAssignableFrom(t) || typeof(IResponse).IsAssignableFrom(t))
-				.Where(t => t.IsClass && !t.Name.EndsWith("Descriptor"))
+				.Where(t => t.IsClass && !isDescriptorRe.IsMatch(t.Name))
 				.ToArray();
+
+			var requestParameters = lowLevelAssembly
+				.GetTypes()
+				.Where(t => t.IsClass && t.Name.EndsWith("RequestParameters"))
+				.ToDictionary(t=>t.Name.Replace("Parameters", ""));
 
 			var definitions = nestInterfaces.Aggregate(
 				TypeScript.Definitions()
 					.WithMemberFormatter(i =>
 					{
 						var declaringType = i.MemberInfo.DeclaringType;
+						if (declaringType.Name.Contains("Request") && requestParameters.ContainsKey(declaringType.Name))
+						{
+							var rp = requestParameters[declaringType.Name];
+							var method = rp.GetMethod(i.MemberInfo.Name);
+							if (method != null)
+							{
 
+							}
+						}
+
+						JsonPropertyAttribute jsonPropertyAttribute;
 						var iface = declaringType.GetInterfaces().FirstOrDefault(ii => ii.Name == "I" + declaringType.Name);
-						if (iface == null)
-						return i.MemberInfo.Name;
-						var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
-						if (ifaceProperty == null)
-						return i.MemberInfo.Name;
-						var jsonPropertyAttribute = ifaceProperty.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
-						if (jsonPropertyAttribute != null)
-						return jsonPropertyAttribute.PropertyName;
+						if (iface != null)
+						{
+							var ifaceProperty = iface.GetProperty(i.MemberInfo.Name);
+							jsonPropertyAttribute = ifaceProperty?.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
+							if (jsonPropertyAttribute != null)
+								return jsonPropertyAttribute.PropertyName;
+						}
+
 						jsonPropertyAttribute = i.MemberInfo.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault() as JsonPropertyAttribute;
 						if (jsonPropertyAttribute != null)
-						return jsonPropertyAttribute.PropertyName;
+							return jsonPropertyAttribute.PropertyName;
 						return i.MemberInfo.Name;
 					})
 				, (def, t) => def.For(t)
