5243ca752b142ad06f33d68f50c949837ddd7a3f
Mpdreamz <mpdreamz@gmail.com>
2016-05-03 00:43:28 +0200

    Figure out why huge bulk responses perform so bad
    Count = 5
        [0]: {00:00:00.0001195} 1 bulk 1/10ms
        [1]: {00:00:00.0011351} 100 bulk 1ms
        [2]: {00:00:00.0096596} 1000 bulk 9ms
        [3]: {00:00:01.1875135} 1000000 bulk 1s
        [4]: {00:00:02.3120415} 1000000 bulk 2s (second time)
    
    tClient.GetInMemoryClient()

---
+++ src/Tests/Document/Multiple/Bulk/BulkResponsePerformance.doc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using Elasticsearch.Net;
+using Nest;
+using Tests.Framework;
+
+namespace Tests.Document.Multiple.Bulk
+{
+	public class BulkResponsePerformance
+	{
+		private static Random Random = new Random(1337);
+		private static IElasticClient Client = TestClient.GetInMemoryClient();
+
+		[U] public void BulkResponseSerializationIsFastEnough()
+		{
+			var serializer = Client.Serializer;
+			var tinyResponse = serializer.SerializeToBytes(this.ReturnBulkResponse(1));
+			var mediumResponse = serializer.SerializeToBytes(this.ReturnBulkResponse(100));
+			var largeResponse = serializer.SerializeToBytes(this.ReturnBulkResponse(1000));
+			var hugeResponse = serializer.SerializeToBytes(this.ReturnBulkResponse(100000));
+
+			var warmup = this.Deserialize(tinyResponse);
+
+			var timings = new List<TimeSpan>();
+			var sw = Stopwatch.StartNew();
+			var tinyBulkResponse = this.Deserialize(tinyResponse);
+			timings.Add(sw.Elapsed);
+			sw.Restart();
+			var mediumBulkResponse = this.Deserialize(mediumResponse);
+			timings.Add(sw.Elapsed);
+			sw.Restart();
+			var largeBulkResponse = this.Deserialize(largeResponse);
+			timings.Add(sw.Elapsed);
+			sw.Restart();
+			var hugeBulkResponse = this.Deserialize(hugeResponse);
+			timings.Add(sw.Elapsed);
+			hugeBulkResponse = this.Deserialize(hugeResponse);
+			timings.Add(sw.Elapsed);
+			sw.Restart();
+		}
+
+		private BulkResponse Deserialize(byte[] response)
+		{
+			using (var ms = new MemoryStream(response))
+				return Client.Serializer.Deserialize<BulkResponse>(ms);
+		}
+
+		private object BulkItemResponse() => new
+		{
+			index = new
+			{
+				_index = "nest-52cfd7aa",
+				_type = "project",
+				_id = "Kuhn LLC",
+				_version = 1,
+				_shards = new
+				{
+					total = 2,
+					successful = 1,
+					failed = 0
+				},
+				created = true,
+				status = 201
+			}
+		};
+
+
+		private object ReturnBulkResponse(int numberOfItems)
+		{
+			return new {
+				took = 276,
+				errors = false,
+				items = Enumerable.Range(0, numberOfItems)
+					.Select(i => BulkItemResponse())
+					.ToArray()
+			};
+		}
+	}
+}
