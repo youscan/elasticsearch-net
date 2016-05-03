385d5a34bf3c6085c0960bcc9d5a75e37129cb3d
Russ Cam <russ.cam@forloop.co.uk>
2016-05-03 14:59:03 +1000

    Map type names along with mappings
    Closes https://github.com/elastic/elasticsearch-net/issues/2072

--- src/Nest/Mapping/Visitor/MappingWalker.cs
+++ src/Nest/Mapping/Visitor/MappingWalker.cs
@@ -15,7 +15,12 @@ namespace Nest
 		public void Accept(IGetMappingResponse response)
 		{
 			if (response == null) return;
-			this.Accept(response.Mapping);
+
+			foreach (var indexMapping in response.IndexTypeMappings)
+			foreach (var typeMapping in indexMapping.Value)
+			{
+				this.Accept(typeMapping.Value);
+			}
 		}
 
 		public void Accept(TypeMapping mapping)
