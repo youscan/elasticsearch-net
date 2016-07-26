e061283f9f1f9c16ce3b4c1072faecf72003ad51
Mpdreamz <mpdreamz@gmail.com>
2016-07-26 10:24:52 +1000

    indendation fixes

--- src/Tests/Framework/Profiling/ProfileTests.cs
+++ src/Tests/Framework/Profiling/ProfileTests.cs
@@ -4,15 +4,15 @@ using Nest;
 
 namespace Tests.Framework.Profiling
 {
-    public abstract class ProfileTests
-    {
-        public static readonly ElasticClient InMemoryClient;
+	public abstract class ProfileTests
+	{
+		public static readonly ElasticClient InMemoryClient;
 
-        static ProfileTests()
-        {
-            var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
-            var connectionSettings = new ConnectionSettings(connectionPool, new InMemoryConnection());
-            InMemoryClient = new ElasticClient(connectionSettings);
-        }
-    }
+		static ProfileTests()
+		{
+			var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
+			var connectionSettings = new ConnectionSettings(connectionPool, new InMemoryConnection());
+			InMemoryClient = new ElasticClient(connectionSettings);
+		}
+	}
 }
--- src/Tests/Framework/Profiling/TimelineAttribute.cs
+++ src/Tests/Framework/Profiling/TimelineAttribute.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace Tests.Framework.Profiling
 {
-    /// <summary>
-    /// identifies a method that should be timeline profiled
-    /// </summary>
-    public class TimelineAttribute : ProfilingAttribute
-    {
-        public TimelineAttribute(int iterations = 1) : base(iterations)
-        {
-        }
-    }
+	/// <summary>
+	/// identifies a method that should be timeline profiled
+	/// </summary>
+	public class TimelineAttribute : ProfilingAttribute
+	{
+		public TimelineAttribute(int iterations = 1) : base(iterations)
+		{
+		}
+	}
 }
