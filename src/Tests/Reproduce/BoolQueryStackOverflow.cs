fe4a52ea075eb7a58eea06911996c280f1f52ede
Greg Marzouka <greg.marzouka@gmail.com>
2016-08-15 08:56:52 -0400

    add test that demonstrates a stack overflow when combining too many bool clauses

---
+++ src/Tests/Reproduce/BoolQueryStackOverflow.cs
@@ -0,0 +1,27 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Framework;
+using Tests.Framework.MockData;
+
+namespace Tests.Reproduce
+{
+	public class BoolQueryStackOverflow
+	{
+		[U]
+		public void LotsOfBoolClauses()
+		{
+			var n = 2000;
+			var container = new QueryContainer();
+			for (int i = 0; i < n; i++)
+			{
+				container |= Query<Project>.Bool(b => b
+					.Must(must => must.Term(p => p.Name, "foo"))
+				);
+			}
+		}
+	}
+}
