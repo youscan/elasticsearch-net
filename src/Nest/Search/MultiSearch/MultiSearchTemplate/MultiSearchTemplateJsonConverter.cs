2ab31e72c2bd07099f5b487eb24ddc51576531af
Greg Marzouka <greg.marzouka@gmail.com>
2016-08-31 18:16:02 -0400

    Support for multi search template

---
+++ src/Nest/Search/MultiSearch/MultiSearchTemplate/MultiSearchTemplateJsonConverter.cs
@@ -0,0 +1,63 @@
+﻿using Elasticsearch.Net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nest
+{
+	internal class MultiSearchTemplateJsonConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType) => true;
+		public override bool CanRead => false;
+		public override bool CanWrite => true;
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var request = value as IMultiSearchTemplateRequest;
+			if (request == null) return;
+			var settings = serializer.GetConnectionSettings();
+			var elasticsearchSerializer = settings.Serializer;
+			if (elasticsearchSerializer == null) return;
+
+			if (request.Operations == null) return;
+
+			foreach (var operation in request.Operations.Values)
+			{
+				var indices = request.Index == null || !request.Index.Equals(operation.Index)
+					? operation.Index
+					: null;
+
+				var types = request.Type == null || !request.Type.Equals(operation.Type)
+					? operation.Type
+					: null;
+
+				var searchType = operation.RequestParameters.GetQueryStringValue<SearchType>("search_type").GetStringValue();
+				if (searchType == "query_then_fetch")
+					searchType = null;
+
+				var header = new
+				{
+					index = (indices as IUrlParameter)?.GetString(settings),
+					type = (types as IUrlParameter)?.GetString(settings),
+					search_type = searchType,
+					preference = operation.Preference,
+					routing = operation.Routing,
+					ignore_unavailable = operation.IgnoreUnavalable
+				};
+
+				var headerBytes = elasticsearchSerializer.SerializeToBytes(header, SerializationFormatting.None);
+				writer.WriteRaw(headerBytes.Utf8String() + "\n");
+				var bodyBytes = elasticsearchSerializer.SerializeToBytes(operation, SerializationFormatting.None);
+				writer.WriteRaw(bodyBytes.Utf8String() + "\n");
+			};
+		}
+	}
+}
